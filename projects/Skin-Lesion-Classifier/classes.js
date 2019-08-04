0a9c8174897d6a5cb4193fb12fe2779a1ba89bae
David S <dsangojinmi26@gmail.com>
2019-08-04 20:57:24 +0100

    Updated article tabs and created article pages

--- projects/Skin-Lesion-Classifier/classes.js
+++
@@ -1,9 +0,0 @@
-const CLASSES ={
-  0: 'Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowen’s disease)',
-  1: 'Basal Cell Carcinoma',
-  2: 'Benign Keratosis',
-  3: 'Dermatofibroma',
-  4: 'Melanoma',
-  5: 'Melanocytic Nevi',
-  6: 'Vascular skin lesion'
-}
