99738b5aced4a95d050ccd123d57f8f246a239d2
David S <dsangojinmi26@gmail.com>
2020-03-25 20:36:20 +0000

    Update platformer.js

--- projects/Javascript/platformer/platformer.js
+++ projects/Javascript/platformer/platformer.js
@@ -24,16 +24,28 @@ function draw() {
     background(245);
     fill(0);
     noStroke();
-    rect(1, 330, 899, 389);
+    rect(1, 330, 898, 388);
 
     image(img, sprite.x, sprite.y, sprite.width, sprite.height);
     spriteMove();
 }
 
 function spriteMove() {
+    //Keyboard controls
+    if (keyIsPressed === true && keyCode === RIGHT_ARROW) {
+        sprite.x += sprite.xspeed;
+    }
+    if (keyIsPressed === true && keyCode === LEFT_ARROW) {
+        sprite.x -= sprite.xspeed;
+    }
+    if (keyIsPressed === true && keyCode === UP_ARROW) {
+        sprite.y -= 10;
+    }
+
+    // Edge detection
     if (sprite.y >= 274) {
         sprite.y += 0;
     } else {
-        sprite.y += 1;
+        sprite.y += sprite.yspeed;
     }
 }
